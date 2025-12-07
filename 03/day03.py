3e1bb2c44cdabaa3baa4f260b6de3256e81d32f9
Lucas Sterzinger <lucassterzinger@gmail.com>
2025-12-06 20:03:14 -0800

    day3 part 1

--- 03/day03.py
+++ 03/day03.py
@@ -1,13 +1,15 @@
-def get_battery_largest_joltage(battery, bank_len):
-    vals = []
-    for b in range(bank_len):
-        val, i = find_largest_value(battery[:-1])
-        battery = battery[i+1:]
+def find_joltage_part1(battery: str) -> int:
 
-def find_largest_value(series):
+    val1, i = find_largest_digit_pair(battery[:-1])
+    val2, _ = find_largest_digit_pair(battery[i+1:])
+    return int(val1 + val2)
+
+
+def find_largest_digit_pair(series: str) -> tuple[str, int]:
+
     # get first value
     reference_val = 0
-    reference_i = 0 
+    reference_i = 0
     for i, val in enumerate(series):
         if int(val) > reference_val:
             reference_val = int(val)
@@ -16,11 +18,13 @@ def find_largest_value(series):
 
 
 if __name__ == '__main__':
-    with open('input.txt') as inf:
+    with open('sample_input.txt') as inf:
         batteries = inf.read().splitlines()
 
-    total_joltage = 0
+    total_joltage_p1 = 0
+
     for battery in batteries:
-        joltage = get_battery_largest_joltage(battery)
-        total_joltage += joltage
-    print(total_joltage)
+        joltage_p1 = find_joltage_part1(battery)
+        total_joltage_p1 += joltage_p1
+
+    print(total_joltage_p1)
